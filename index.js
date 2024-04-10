d2a6896e0d1dd45a0d400934c2a82ea6447e905f
jennadunford <jenna.dunford@bbd.co.za>
2024-04-10 07:39:53 +0200

    update

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const selectedStyle = core.getInput('style');
+  console.log(`Chosen style ${selectedStyle}!`);
+  const docNames = core.getInput('name-of-docs');
+  console.log(`Chosen style ${docNames}!`);
+  const filesInput = core.getInput('files-input');
+  console.log(`Chosen style ${filesInput}!`);
+  const generatedDocs = ["Yo","Yo"];
+  core.setOutput("generated-docs", generatedDocs);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
+}
+
+
+function getBase64(file) {
+  var reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = function () {
+    console.log(reader.result);
+  };
+  reader.onerror = function (error) {
+    console.log('Error: ', error);
+  };
 }
